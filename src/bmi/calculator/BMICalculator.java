5592ce17e343dbf7a86deb3ba2aff57e74783475
22500 <22500@MININT-58O3Q68.columbushs.com>
2019-10-04 14:42:11 -0400

    Done

--- src/bmi/calculator/BMICalculator.java
+++ src/bmi/calculator/BMICalculator.java
@@ -13,24 +13,32 @@ package bmi.calculator;
 import java.util.Scanner;
 
 public class BMICalculator {
-
-    /**
-     * @param args the command line arguments
-     */
-    private static Scanner in = new Scanner(System.in);
-    
+   
+   
     public static void main(String[] args) {
-        Scanner scannerMachine = scan
+       
+        Scanner input = new Scanner(System.in);
+        System.out.print("Welcome to the Body Mass Index calculator \n"
+                        +"Please print out your information and press enter \n"
+                        +"Thank you \n");
+        System.out.print("Your height in inches ");
+        double heightIn = input.nextDouble();
+        System.out.println("");
+        System.out.print("Your height in feet ");
+        double heightFt = input.nextDouble();
+        System.out.println("");
+        System.out.print("Your weight in pounds ");
+        double weightLbs = input.nextDouble();
+        System.out.println("");
+        double bmi = (703*weightLbs/(heightIn*heightIn));
+        System.out.print("Your BMI is " +bmi);
+       
+        /*Formula in pounds and inches=
+        *703 x weight (lbs) / [height (in)]270
         
-        System.out.println("Your weight in inches:");
-        
-        // Prompt for height
-        
-        // invoke the calculateBMI method
-        
-        
+        */        
     }
-    
-    // calculateBMI method definition
-    
+   
+   
 }
+
